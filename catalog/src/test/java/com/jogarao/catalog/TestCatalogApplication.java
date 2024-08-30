7a353cfebd4210c534f32b7e64ae7acd14db8131
Jogarao <jogarao@effecientdeliveries.com>
2024-08-30 17:10:09 +0530

    Catalogue setup

---
+++ catalog/src/test/java/com/jogarao/catalog/TestCatalogApplication.java
@@ -0,0 +1,11 @@
+package com.jogarao.catalog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
